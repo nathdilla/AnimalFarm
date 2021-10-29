6badf08b0fd09ad5f9dbab7fcbd6e66088dd91ad
Nathan Dilla <nathcdilla@protonmail.com>
2021-10-29 12:11:39 -0700

    Initial Commit

---
+++ AnimalFarm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AnimalFarm
+//
+//  Created by Nathan Dilla on 10/29/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
